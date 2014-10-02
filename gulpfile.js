48713a8449c96fa5a442b163ad4d204ac5d63d6e
Ariel Schiavoni <arielschiavoni@gmail.com>
2014-10-02 15:51:36 +0200

    Improve gulp tasks, watch file changes to execute cordoba:prepare

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,46 @@
 var gulp = require('gulp');
-var jshint = require('gulp-jshint');
+var plugins = require('gulp-load-plugins')();
 
-gulp.task('jshint', function() {
-  return gulp.src(['gulpfile.js'])
-	  .pipe(jshint('.jshintrc'))
-	  .pipe(jshint.reporter('jshint-stylish'));
+require('gulp-help')(gulp);
+
+var paths = {
+	scripts: [
+		'gulpfile.js',
+		'www/js/*.js'
+	],
+	www: [
+		'www/index.html',
+		'www/**/*.{css,js,png}'
+	]
+};
+
+gulp.task('jshint', 'Lint all Javascript files.', function() {
+  return gulp.src(paths.scripts)
+	  .pipe(plugins.jshint('.jshintrc'))
+	  .pipe(plugins.jshint.reporter('jshint-stylish'));
 });
 
-gulp.task('default', ['jshint']);
+gulp.task('watch', 'Watch files and excecute -> [jshint, cordova:prepare]', function() {
+  gulp.watch(paths.scripts, ['jshint']);
+  gulp.watch(paths.www, ['cordova:prepare']);
+});
+
+gulp.task('cordova:prepare', 'Copies files for all platforms so that the project is ready to build with each SDK', function (done) {
+
+	var path = require('path');
+	var spawn = require('child_process').spawn;
+
+  var cmd = path.resolve('./node_modules/cordova/bin', 'cordova');
+  var child = spawn(cmd, ['prepare']);
+
+  child.stdout.on('data', function (data) {
+    console.log.writeln(data);
+  });
+  child.stderr.on('data', function (data) {
+    console.log.error(data);
+  });
+  child.on('close', function (code) {
+    done(code);
+  });
+
+});
